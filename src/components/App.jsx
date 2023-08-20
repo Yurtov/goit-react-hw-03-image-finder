9a9a534b26f21deabd45eec6d3563dab62c2d60e
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-08-20 11:19:49 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,102 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import toast, { Toaster } from 'react-hot-toast';
+import ScrollToTop from 'react-scroll-to-top';
+import { Button } from './Button/Button';
+import { fetchImage } from '../api/Api.js';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { GlobalStyle } from 'GlobalStyle';
+import { Layout, SpinerMargin } from './Loyaut';
+import { Spiner } from './Spinner/Spiner';
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+    total: null,
+    error: null,
+    loading: false,
+  };
+
+  componentDidMount() {}
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      const { query, page } = this.state;
+      const indexOfQuery = query.indexOf('/') + 1;
+      const valueOfQuery = query.slice(indexOfQuery);
+      try {
+        this.setState({ loading: true });
+        if (valueOfQuery === '') {
+          return toast.error(
+            'You cannot send an empty request, please write something'
+          );
+        }
+        const { hits, total } = await fetchImage(page, valueOfQuery);
+
+        if (total === 0 || !hits) {
+          return toast.error('Didnt find it, try again');
+        }
+
+        this.setState({
+          images: page === 1 ? hits : [...prevState.images, ...hits],
+          total: total,
+        });
+      } catch (error) {
+        this.setState({
+          error: error,
+        });
+      } finally {
+        this.setState({
+          loading: false,
+        });
+      }
+    }
+  }
+
+  heanleChange = e => {
+    e.preventDefault();
+    const newQuery = e.target.elements.query.value.trim();
+    const reqId = nanoid();
+    this.setState({
+      query: `${reqId}/${newQuery}`,
+      images: [],
+      page: 1,
+      total: null,
+    });
+    e.target.reset();
+  };
+
+  heandleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  render() {
+    const { images, loading, total } = this.state;
+
+    return (
+      <Layout>
+        <Searchbar onChange={this.heanleChange} />
+        {loading ? (
+          <SpinerMargin>
+            <Spiner />
+          </SpinerMargin>
+        ) : (
+          <ImageGallery images={images} />
+        )}
+        {total && total !== images.length && (
+          <Button onClick={this.heandleLoadMore} />
+        )}
+        <div style={{ marginTop: '150vh' }} />
+        <ScrollToTop smooth />
+        <Toaster />
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
